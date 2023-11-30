6b7fa9eed679698821caae43fff32c58b86584c5
Tugba Esat <tugbaesat007@gmail.com>
2023-11-29 18:49:39 -0800

    todo app created with vuejs

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Vue Todos</h1>
+    <p>Vue Todos is a CRUD todo tracking application.</p>
+    <p>
+      Track your daily tasks using this app, which allows you to save, mark as completed, edit, and delete them.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ src/views/TodosView.vue
@@ -0,0 +1,115 @@
+<script setup>
+import TodoCreator from "../components/TodoCreator.vue";
+import { uid } from "uid";
+import { ref, watch, computed } from "vue";
+import TodoItem from "../components/TodoItem.vue";
+import { Icon } from "@iconify/vue";
+
+const todoList = ref([]);
+
+watch(
+  todoList,
+  () => {
+    setTodoListLocalStorage();
+  },
+  {
+    deep: true,
+  }
+);
+
+const todoCompleted = computed(() => {
+  return todoList.value.every((todo) => todo.isCompleted);
+});
+
+const fetchTodoList = () => {
+  const savedTodoList = JSON.parse(localStorage.getItem("todoList"));
+  if (savedTodoList) {
+    todoList.value = savedTodoList;
+  }
+};
+fetchTodoList();
+
+const setTodoListLocalStorage = () => {
+  localStorage.setItem("todoList", JSON.stringify(todoList.value));
+};
+
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null,
+  });
+};
+const toggleTodoComplete = (todoPos) => {
+  todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+};
+const toggleEditTodo = (todoPos) => {
+  todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+};
+const updateTodo = (todoVal, todoPos) => {
+  todoList.value[todoPos].todo = todoVal;
+};
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+};
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem
+        v-for="(todo, index) in todoList"
+        :todo="todo"
+        :index="index"
+        @toggle-complete="toggleTodoComplete"
+        @edit-todo="toggleEditTodo"
+        @update-todo="updateTodo"
+        @deleteTodo="deleteTodo"
+      />
+    </ul>
+    <p class="todos-msg" v-else>
+      <span>You have no todo's to complete! Add one!</span
+      ><Icon icon="emojione:sad-but-relieved-face" width="22" />
+    </p>
+    <p v-if="todoCompleted && todoList.length > 0" class="todos-msg">
+      <Icon icon="emojione:party-popper" color="green" width="22" /><span
+        >You have completed all your todos!</span
+      >
+      <Icon icon="emojione:party-popper" color="green" width="22" />
+    </p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+</style>
